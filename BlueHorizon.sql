b5a7a6e4c6689017975bc996cd3ae8fb7689a36b
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-05-27 19:35:38 -0300

    Exclusão de possíveis tabelas

--- BlueHorizon.sql
+++ BlueHorizon.sql
@@ -3,3 +3,12 @@
 ---Gustavo Argüello Bertacci - 551304
 ---Luiz Fillipe Farias - 99519
 ---Rafaella Monique do Carmo Bastos - 552425
+
+---Exclusão de possíveis tabelas
+drop table Voluntario_Pessoa cascade constraints;
+drop table Voluntario_Endereco cascade constraints;
+drop table Voluntario_Perfil cascade constraints;
+drop table Pontos_Coleta cascade constraints;
+drop table Tipos_Lixos cascade constraints;
+drop table Recebimento_Lixo cascade constraints;
+drop table Situacao_Praia cascade constraints;
